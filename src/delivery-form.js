43d0f59e4acbd232778c06c37c1013de5492655a
Derek Houck <derek@derekhouck.com>
2019-01-31 11:21:34 -0800

    Add fetch request

--- src/delivery-form.js
+++ src/delivery-form.js
@@ -1,51 +1,98 @@
 import React from 'react';
 import './delivery-form.css';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, SubmissionError } from 'redux-form';
 import { required, trackingLength, mustBeNumber } from './validators';
 import Input from './components/input';
 
-export function DeliveryForm(props) {
-  return (
-    <div className="delivery-form">
-      <h2>Report a problem with your delivery</h2>
-      <form onSubmit={props.handleSubmit((values) => console.log(values))}>
-        <div>
-          <Field 
-            component={Input} 
-            element="input" 
-            label="Tracking Number"
-            validate={[required, trackingLength, mustBeNumber]} 
-            type="text" 
-            name="trackingNumber" 
-            id="trackingNumber" />
-        </div>
-        <div>
-          <Field 
-            component={Input} 
-            element="select" 
-            label="What is your issue?"
-            validate={required} 
-            name="issue" 
-            id="issue">
-            <option value="not-delivered">My delivery hasn't arrived</option>
-            <option value="wrong-item">The wrong item was delivered</option>
-            <option value="missing-part">Part of my order was missing</option>
-            <option value="damage">Some of my order arrived damaged</option>
-            <option value="other">Other (give details below)</option>
-          </Field>
-        </div>
-        <div>
-          <Field 
-            component={Input} 
-            element="textarea" 
-            label="Give more details"
-            name="details" 
-            id="details"></Field>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+export class DeliveryForm extends React.Component {
+  onSubmit(values) {
+    return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          if (
+            res.headers.has('content-type') &&
+            res.headers
+              .get('content-type')
+              .startsWith('application/json')
+          ) {
+            // It's a nice JSON error returned by us, so decode it
+            return res.json().then(err => Promise.reject(err));
+          }
+          // It's a less informative error returned by express
+          return Promise.reject({
+            code: res.status,
+            message: res.statusText
+          });
+        }
+        return;
+      })
+      .catch(err => {
+        const { reason, message, location } = err;
+        if (reason === 'ValidationError') {
+          // Convert ValidationErrors into SubmissionErrors for Redux Form
+          return Promise.reject(
+            new SubmissionError({
+              [location]: message
+            })
+          );
+        }
+        return Promise.reject(
+          new SubmissionError({
+            _error: 'Error submitting message'
+          })
+        );
+      });
+  }
+
+  render() {
+    return (
+      <div className="delivery-form">
+        <h2>Report a problem with your delivery</h2>
+        <form onSubmit={this.props.handleSubmit((values) => this.onSubmit(values))}>
+          <div>
+            <Field
+              component={Input}
+              element="input"
+              label="Tracking Number"
+              validate={[required, trackingLength, mustBeNumber]}
+              type="text"
+              name="trackingNumber"
+              id="trackingNumber" />
+          </div>
+          <div>
+            <Field
+              component={Input}
+              element="select"
+              label="What is your issue?"
+              validate={required}
+              name="issue"
+              id="issue">
+              <option value="not-delivered">My delivery hasn't arrived</option>
+              <option value="wrong-item">The wrong item was delivered</option>
+              <option value="missing-part">Part of my order was missing</option>
+              <option value="damage">Some of my order arrived damaged</option>
+              <option value="other">Other (give details below)</option>
+            </Field>
+          </div>
+          <div>
+            <Field
+              component={Input}
+              element="textarea"
+              label="Give more details"
+              name="details"
+              id="details"></Field>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default reduxForm({
