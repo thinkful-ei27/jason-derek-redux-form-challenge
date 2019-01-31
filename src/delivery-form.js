91c4925adcc800c6f541f41ef924ea2c33e9df67
Derek Houck <derek@derekhouck.com>
2019-01-31 08:42:14 -0800

    Render static form

--- src/delivery-form.js
+++ src/delivery-form.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import './delivery-form.css';
 
 export default function DeliveryForm(props) {
-  return (<div>form will be here</div>);
+  return (
+    <div class="delivery-form">
+      <h2>Report a problem with your delivery</h2>
+      <form>
+        <div>
+          <label htmlFor="trackingNumber">Tracking Number</label>
+          <input type="text" name="trackingNumber" id="trackingNumber" />
+        </div>
+        <div>
+          <label htmlFor="issue">What is your issue?</label>
+          <select name="issue" id="issue">
+            <option value="not-delivered">My delivery hasn't arrived</option>
+            <option value="wrong-item">The wrong item was delivered</option>
+            <option value="missing-part">Part of my order was missing</option>
+            <option value="damage">Some of my order arrived damaged</option>
+            <option value="other">Other (give details below)</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="details">Give more details</label>
+          <textarea name="details" id="details" cols="30" rows="10"></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
